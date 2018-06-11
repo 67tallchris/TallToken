70993d501f5a623262a3f2bf3270189ba88b8142
Christopher Porter <chris@Christophers-MacBook-Pro-2.local>
2018-06-11 15:10:37 -0700

    Set parameters for TallToken

--- contracts/TallToken.sol
+++ contracts/TallToken.sol
@@ -3,5 +3,8 @@ pragma solidity ^0.4.24;
 import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
 contract TallToken is StandardToken {
-
+  string public name = 'TallToken';
+  string public symbol = 'TALL';
+  uint8 public decimals = 18;
+  uint public INITIAL_SUPPLY = 21000000;
 }
